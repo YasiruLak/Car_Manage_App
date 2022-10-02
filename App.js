d58e7f92c36d5d97547949266a5ad8092570d593
YasiruLak <yasirudahanayaka19@gmail.com>
2022-10-02 19:44:03 +0530

    Login and Registration Page Done

---
+++ App.js
@@ -0,0 +1,22 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import SignInScreen from './screen/SignInScreen';
+import SignUpScreen from './screen/SignUpScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{header: () => null}}>
+        <Stack.Screen name="SignIn" component={SignInScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
